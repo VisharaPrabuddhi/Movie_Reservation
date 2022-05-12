20e20cf1468c8b505ce999bdebe28f7df147a968
HirushaRukmal <hirusharukmal266@gmail.com>
2022-05-12 13:23:45 +0530

    fix(Login): Updates to login

--- Frontend/movie-reservation/src/components/Login.js
+++ Frontend/movie-reservation/src/components/Login.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../asset/css/Navigation.css";
 import "../asset/css/Movie.css";
+import "../asset/css/Login.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Headers from "./Header";
 import Footer from "./Footer";
-import image from "../asset/image/login.jpg";
 
 function Login() {
   return (
-    <div className="image">
+    <div className="pic">
       <Headers />
       <div className="box">
         <h3>Welcome Back!</h3>
@@ -22,7 +22,7 @@ function Login() {
             type="email"
             class="form-control"
             id="exampleFormControlInput1"
-            placeholder="name@example.com"
+            placeholder="Enter email"
           />
         </div>
         <div class="mb-3">
@@ -33,15 +33,18 @@ function Login() {
             type="password"
             class="form-control"
             id="exampleFormControlInput1"
+            placeholder="Enter password"
           />
         </div>
         <div class="d-grid gap-2 d-md-block">
           <button class="btn btn-primary" type="button">
-            Button
+            Login
           </button>
         </div>
         <br />
-        <a href="./Register">Signup</a>
+        <p className="p1">
+          Don't have an account<a href="./Register"> Signup</a>?
+        </p>
       </div>
       <Footer />
     </div>
