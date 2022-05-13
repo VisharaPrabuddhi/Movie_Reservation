32b5c55627bb173a00cf1deeb20c3cc0352d0ff3
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-05-14 01:50:44 +0530

    movie backend services implemented

---
+++ Backend/ds_assignment/src/main/java/com/movie_reservation/ds_assignment/model/Movie.java
@@ -0,0 +1,130 @@
+package com.movie_reservation.ds_assignment.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+@Document(collection = "movie")
+@Data
+public class Movie {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String genre;
+    private int rating;
+    private Date releaseDate;
+    private String language;
+    private List tags;
+    private String director;
+    private List cast;
+    private Boolean available;
+
+    public Movie(String id, String name, String description, String genre, int rating, Date releaseDate, String language, List tags, String director, List cast, Boolean available) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.genre = genre;
+        this.rating = rating;
+        this.releaseDate = releaseDate;
+        this.language = language;
+        this.tags = tags;
+        this.director = director;
+        this.cast = cast;
+        this.available = available;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public List getTags() {
+        return tags;
+    }
+
+    public void setTags(List tags) {
+        this.tags = tags;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public List getCast() {
+        return cast;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public void setCast(List cast) {
+        this.cast = cast;
+    }
+
+
+}
